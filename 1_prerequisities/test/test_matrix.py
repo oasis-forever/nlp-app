1e86489d2eefbe85834f8ed8211fbec877c0d261
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2022-02-15 00:22:03 +0900

    Commit TestVector class

---
+++ 1_prerequisities/test/test_matrix.py
@@ -0,0 +1,19 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_array_equal
+import sys
+sys.path.append('./1_prerequisities/src')
+from numpy_array import NumpyArray
+
+class TestMatrix(unittest.TestCase):
+    def setUp(self):
+        matrix = [
+            [ 1,  2,  3,  4],
+            [ 5,  6,  7,  8],
+            [ 9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ]
+        self.matrix = NumpyArray(matrix)
+
+if __name__ == '__main__':
+    unittest.main()
